fa80d59fdedca9238e75ec03509e55228c90a974
Dennis Schwertel <dennisschwertel@gmail.com>
2019-08-14 10:54:06 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+INSTALLED_ADDONS = [
+    # <INSTALLED_ADDONS>  # Warning: text inside the INSTALLED_ADDONS tags is auto-generated. Manual changes will be overwritten.
+    'aldryn-addons',
+    'aldryn-django',
+    'aldryn-sso',
+    'aldryn-common',
+    # </INSTALLED_ADDONS>
+]
+
+import aldryn_addons.settings
+aldryn_addons.settings.load(locals())
+
+
+# all django settings can be altered here
+
+INSTALLED_APPS.extend([
+    # add your project specific apps here
+])
